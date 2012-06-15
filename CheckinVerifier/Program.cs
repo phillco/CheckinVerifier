d2183bf8175bccca57a67c92b4e76d75d9a8d254
Phillip Cohen <phillip.cohen@10gen.com>
2012-06-15 11:39:33 -0700

    Refactor....much simpler.

--- CheckinVerifier/Program.cs
+++ CheckinVerifier/Program.cs
@@ -16,14 +16,17 @@ namespace CheckinVerifier
         //
         //===========================================
 
-        const string BaseFolder = @"Source\";
-        const string SampleFolder = @"Samples\SampleMvcApp\";
+        static class BaseProject
+        {
+            public const string DeployFolder = "Source\\AzureDeploy";
+            public const string SetupFolder = "Source\\Setup";
+        }
 
-        static string BaseDeployFolder = Path.Combine( BaseFolder, @"AzureDeploy\" );
-        static string SampleDeployFolder = Path.Combine( SampleFolder, @"SampleAzureDeploy\" );
-
-        const string ServiceDefinitionFilename = "ServiceDefinition.csdef";
-        const string ServiceConfigurationFilename = "ServiceConfiguration.Local.cscfg";
+        static class SampleProject
+        {
+            public const string DeployFolder = "Samples\\SampleMvcApp\\SampleAzureDeploy";
+            public const string SetupFolder =  "Samples\\SampleMvcApp\\Setup"; 
+        }    
 
         //===========================================
         //
@@ -37,42 +40,54 @@ namespace CheckinVerifier
             // Step 1: Verify ServiceDefinition between Source/AzureDeploy and Sample/SampleAzureDeploy.
             //
             {
-                var baseServiceDefinition = Path.Combine( BaseDeployFolder, ServiceDefinitionFilename );
-                var sampleServiceDefinition = Path.Combine( SampleDeployFolder, ServiceDefinitionFilename );
-                PrintResult( ServiceDefinitionFilename, VerifyFirstElement( baseServiceDefinition, sampleServiceDefinition ) );
+                var file = "ServiceDefinition.csdef";
+                var baseServiceDefinition = Path.Combine( BaseProject.DeployFolder, file );
+                var sampleServiceDefinition = Path.Combine( SampleProject.DeployFolder, file );
+                PrintResult( file, VerifyFirstElement( baseServiceDefinition, sampleServiceDefinition ) );
             }
 
             //
             // Step 2: Verify the local ServiceConfiguration between Source/AzureDeploy and Sample/SampleAzureDeploy.
             //
             {
-                var baseDoc = Path.Combine( BaseDeployFolder, ServiceConfigurationFilename );
-                var sampleDoc = Path.Combine( SampleDeployFolder, ServiceConfigurationFilename );
-                PrintResult( ServiceConfigurationFilename, VerifyFirstElement( baseDoc, sampleDoc ) );
+                var file = "ServiceConfiguration.Local.cscfg";
+                var baseDoc = Path.Combine( BaseProject.DeployFolder, file );
+                var sampleDoc = Path.Combine( SampleProject.DeployFolder, file );
+                PrintResult( file, VerifyFirstElement( baseDoc, sampleDoc ) );
             }
 
+            //
+            // Step 3: Verify the cloud ServiceConfiguration between the two setup dirs.
+            //
+            {
+                var file = "ServiceConfiguration.Cloud.cscfg.ref";
+                var basePath = Path.Combine( BaseProject.SetupFolder, file );
+                var samplePath = Path.Combine( SampleProject.SetupFolder, file );
+                PrintResult( file, VerifyFirstElement( basePath, samplePath ) );
+            }
 
             Console.ReadKey( );
         }
 
         static void PrintResult( string test, bool result )
         {
-            Console.Write( String.Format( "Verifying {0, -50}", test + ".." ) );
+            Console.Write( String.Format( "Verifying {0, -60}", test + ".." ) );
 
-            // Print the result.
+            // Print the result.            
             if ( result )
-            {
+            {                
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write( "\t\t[ OK ]" );
+                Console.Write( "[ OK ]" );
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write( "\t\t[ FAIL ]" );
+                Console.Write( "[ FAIL ]" );
             }
-
-            Console.WriteLine( );
+            
+            Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine( );
         }
 
         static bool VerifyFirstElement( string basePath, string samplePath )
